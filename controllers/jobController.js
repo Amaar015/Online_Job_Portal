028bbb3dfd0bf2c820a5354e5d832f137f19f15d
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2024-03-28 12:06:16 +0500

    Completed Online job portal

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -14,57 +14,68 @@ export const createJobCollector = async (req, res, next) => {
 
 // get all jobs
 export const getJobController = async (req, res, next) => {
-  //  get these values from local url
-  const { status, workType, search, sort } = req.query;
-  //  conditions for searching job filters
-  const queryObject = {
-    createdBy: req.user.userId,
-  };
-  // logic for filters
-  if (status && status !== "all") {
-    queryObject.status = status;
+  try {
+    const jobs = await jobModel.find();
+    console.log(jobs);
+    res.status(200).json({
+      //   totalJobs,
+      jobs,
+      //   numOfPage,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: "Opps! No job found",
+      error,
+    });
   }
-  if (workType && workType !== "all") {
-    queryObject.workType = workType;
-  }
-  if (search) {
-    queryObject.position = { $regex: search, $options: "i" };
-  }
-
-  // get filtered job
-  let queryResult = jobModel.find(queryObject);
-  // sorting
-  if (sort === "latest") {
-    queryResult = queryResult.sort("-createdAt");
-  }
-  if (sort === "oldest") {
-    queryResult = queryResult.sort("createdAt");
-  }
-  if (sort === "a-z") {
-    queryResult = queryResult.sort("position");
-  }
-  if (sort === "z-a") {
-    queryResult = queryResult.sort("-position");
-  }
-
-  //  pagination from backend
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
-
-  queryResult = queryResult.skip(skip).limit(limit);
-  // total jobs count
-  const totalJobs = await jobModel.countDocuments(queryResult);
-  const numOfPage = Math.ceil(totalJobs / limit);
-
-  const job = await queryResult;
-  // const job = await jobModel.find({ createdBy: req.user.userId });
-  res.status(200).json({
-    totalJobs,
-    job,
-    numOfPage,
-  });
+  // const { status, workType, search, sort } = req.query;
+  // //conditons for searching filters
+  // const queryObject = {
+  //   createdBy: req.user.userId,
+  // };
+  // //logic filters
+  // if (status && status !== "all") {
+  //   queryObject.status = status;
+  // }
+  // if (workType && workType !== "all") {
+  //   queryObject.workType = workType;
+  // }
+  // if (search) {
+  //   queryObject.position = { $regex: search, $options: "i" };
+  // }
+  // let queryResult = jobsModel.find(queryObject);
+  // //sorting
+  // if (sort === "latest") {
+  //   queryResult = queryResult.sort("-createdAt");
+  // }
+  // if (sort === "oldest") {
+  //   queryResult = queryResult.sort("createdAt");
+  // }
+  // if (sort === "a-z") {
+  //   queryResult = queryResult.sort("position");
+  // }
+  // if (sort === "z-a") {
+  //   queryResult = queryResult.sort("-position");
+  // }
+  //pagination
+  // const page = Number(req.query.page) || 1;
+  // const limit = Number(req.query.limit) || 10;
+  // const skip = (page - 1) * limit;
+  // queryResult = queryResult.skip(skip).limit(limit);
+  // //jobs count
+  // const totalJobs = await jobsModel.countDocuments(queryResult);
+  // const numOfPage = Math.ceil(totalJobs / limit);
+  // const jobs = await queryResult;
+  // // const jobs = await jobsModel.find({ createdBy: req.user.userId });
+  // res.status(200).json({
+  //   totalJobs,
+  //   jobs,
+  //   numOfPage,
+  // });
 };
+
 // ======= UPDATE JOBS ===========
 export const updateJobController = async (req, res, next) => {
   const { id } = req.params;
